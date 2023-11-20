ef5266077aea5115c09075eeae0780c7866133de
X3NO <X3NO@disroot.org>
2023-11-20 02:31:34 +0100

    All functionality has been added and tested

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ const multipliers = {
   extreme: 1.9,
 };
 
+const timetable = {
+  days: 1,
+  weeks: 7,
+  months: 31,
+  years: 365,
+};
+
 function formSerialize(form) {
   return new URLSearchParams(form).toString();
 }
@@ -32,28 +39,33 @@ function formDeserialize(form, data) {
 }
 
 class Person {
-  constuctor(weight, height, age, gender, activity_level) {
-    console.log("chuj")
+  constructor(weight, height, age, gender, activity_level) {
     this.weight = weight;
     this.height = height;
     this.age = age;
     this.gender = gender;
     this.activity_level = activity_level;
-    console.log(weight)
-    console.log(this)
   }
 
   bmi() {
     return (
       Math.round(
-        (this.weight / this.height / this.height + Number.EPSILON) * 100
-      ) / 100
+        (this.weight / ((this.height / 100) * (this.height / 100)) +
+          Number.EPSILON) *
+          10
+      ) / 10
     );
   }
 
   bmi_normal_weight_range() {
-    let min = 18.5 * this.height * this.height;
-    let max = 24.9 * this.height * this.height;
+    let min =
+      Math.round(
+        18.5 * (this.height / 100) * (this.height / 100) * 100 + Number.EPSILON
+      ) / 100;
+    let max =
+      Math.round(
+        24.9 * (this.height / 100) * (this.height / 100) * 100 + Number.EPSILON
+      ) / 100;
     return { min, max };
   }
 
@@ -96,7 +108,7 @@ class Person {
     let ibw = 0.9 * (this.height - 152);
     this.gender === "male" ? (ibw += 50) : (ibw += 45.5);
 
-    return ibw;
+    return Math.round((ibw + Number.EPSILON) * 100) / 100;
   }
 
   bmr() {
@@ -113,22 +125,110 @@ class Person {
 
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
-  fd = new FormData(e.target);
+  let fd = new FormData(e.target);
   // store in local storage
   localStorage.removeItem("person");
   localStorage.setItem("person", formSerialize(fd));
 
-  p = new Person(
+  const p = new Person(
     fd.get("weight"),
     fd.get("height"),
     fd.get("age"),
     fd.get("gender"),
     fd.get("activity_level")
   );
-  console.log(p);
-  document.getElementById("bmi").innerHTML = p.bmi();
-  document.getElementById("bmi_category").innerHTML = p.bmi_description();
 
+  let only_pounds = document.getElementById("only_pounds").checked;
+
+  document.getElementById("bmi").innerText = p.bmi();
+  document.getElementById("bmi_category").innerText = p.bmi_description();
+  let normal_bmi_range = p.bmi_normal_weight_range();
+  document.getElementById("bmi_healthy_weight_range").innerText =
+    normal_bmi_range.min + "kg - " + normal_bmi_range.max + "kg";
+  normal_bmi_range_imperial = {
+    min: kg_to_imperial(normal_bmi_range.min, only_pounds),
+    max: kg_to_imperial(normal_bmi_range.max, only_pounds),
+  };
+  document.getElementById("bmi_healthy_weight_range_imperial").innerText =
+    normal_bmi_range_imperial.max.stones !== 0
+      ? normal_bmi_range_imperial.min.stones +
+        " stone " +
+        normal_bmi_range_imperial.min.pounds +
+        " pounds - " +
+        normal_bmi_range_imperial.max.stones +
+        " stone " +
+        normal_bmi_range_imperial.max.pounds +
+        " pounds"
+      : normal_bmi_range_imperial.min.pounds +
+        " pounds - " +
+        normal_bmi_range_imperial.max.pounds +
+        " pounds";
+  document.getElementById("ibw").innerText = p.ibw() + "kg";
+  let ibw_imperial = kg_to_imperial(p.ibw(), only_pounds);
+  document.getElementById("ibw_imperial").innerText =
+    ibw_imperial.stones !== 0
+      ? ibw_imperial.stones + " stone " + ibw_imperial.pounds + " pounds"
+      : ibw_imperial.pounds + " pounds";
+  document.getElementById("bmr").innerText =
+    Math.round(p.bmr() + Number.EPSILON) + " kcal";
+  document.getElementById("tdee").innerText =
+    Math.round(p.tdee() + Number.EPSILON) + " kcal";
+  let calorie_per_day = p.tdee();
+  let weight_diff = p.weight - parseInt(fd.get("desired_weight"));
+  let calories_to_the_goal = weight_diff * CALORIES_IN_KG_OF_FAT;
+  let calories_diff;
+  switch (fd.get("desired_method")) {
+    case "deficit":
+      calories_diff = parseFloat(fd.get("desired_deficit"));
+      break;
+    case "time":
+      let days_to_the_goal =
+        parseInt(fd.get("desired_time")) * timetable[fd.get("desired_time_unit")];
+      calories_diff = calories_to_the_goal / days_to_the_goal;
+      break;
+    default:
+      alert("error, unknown deficit method");
+      throw new Error("unknown deficit method");
+  }
+  let calorie_per_cheatday = calorie_per_day;
+  if (p.weight > fd.get("desired_weight")) {
+    document.getElementById("surplus_or_deficit").innerText = "deficit";
+    calorie_per_day -= calories_diff;
+    if (fd.has("cheatday") && fd.get("cheatday_value") > 0) {
+      calorie_per_day -= fd.get("cheatday_value");
+      calorie_per_cheatday += fd.get("cheatday_value") * 7;
+    }
+  } else {
+    document.getElementById("surplus_or_deficit").innerText = "surplus";
+    calorie_per_day = calorie_per_day + calories_diff;
+    if (fd.has("cheatday") && fd.get("cheatday_value") > 0) {
+      calorie_per_day += fd.get("cheatday_value");
+      calorie_per_cheatday -= fd.get("cheatday_value") * 7;
+    }
+  }
+  document.getElementById("calorie_diff").innerText = Math.abs(calories_diff);
+  document.getElementById("overcalorie_warning").hidden = !(
+    calorie_per_cheatday > p.tdee() && p.weight > fd.get("desired_weight")
+  );
+  document.getElementById("calories_per_day").innerText =
+    Math.round(calorie_per_day) + " kcal";
+  document.getElementById("calories_per_cheatday").innerText =
+    Math.round(calorie_per_cheatday) + " kcal";
+  let days_to_the_goal = Math.round(
+    calories_to_the_goal / (p.tdee() - calorie_per_day)
+  );
+  let today = new Date();
+  let time_to_the_goal = new Date(
+    today.setDate(today.getDate() + days_to_the_goal)
+  );
+  document.getElementById("time_to_goal").innerText =
+    time_to_the_goal.toLocaleString("en-GB", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+
+  document.getElementById("cheatdays").hidden = !fd.has("cheatday") || fd.get("cheatday_value") <= 0;
   document.getElementById("output").hidden = false;
 });
 
